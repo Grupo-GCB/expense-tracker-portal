ffd7cd86f6fc723e3a9d6a876033eec650b216d5
charlenefialho <fialhocharlene@gmail.com>
2023-07-26 19:06:43 -0300

    refactor: adds barrer exports and change text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@
 
 import { useUser } from "@auth0/nextjs-auth0/client";
 
-import Home from "@/components/Home";
-import LandingPage from "@/components/LandingPage";
+import { Home, LandingPage } from "@/components";
 
 export default function Page() {
   const { user, error, isLoading } = useUser();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>Carregando...</div>;
+
   if (error) return <div>{error.message}</div>;
 
   if (user) {
