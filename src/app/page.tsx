0e6c6fa5bb28ab2908ece6ea510de6cd1e84aa4d
AugustoCVS <guty.vertolis@gmail.com>
2023-07-26 19:11:31 -0300

    refactor: adds format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
-"use client";
+'use client'
 
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { useUser } from '@auth0/nextjs-auth0/client'
 
-import { Home, LandingPage } from "@/components";
+import { Home, LandingPage } from '@/components'
 
 export default function Page() {
-  const { user, error, isLoading } = useUser();
+  const { user, error, isLoading } = useUser()
 
-  if (isLoading) return <div>Carregando...</div>;
+  if (isLoading) return <div>Carregando...</div>
 
-  if (error) return <div>{error.message}</div>;
+  if (error) return <div>{error.message}</div>
 
   if (user) {
     return (
       <>
         <Home user={user} />
       </>
-    );
+    )
   }
 
-  return <LandingPage />;
+  return <LandingPage />
 }
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,15 +1,15 @@
-import { UserProfile } from "@auth0/nextjs-auth0/client";
+import { UserProfile } from '@auth0/nextjs-auth0/client'
 
 interface IUser {
-  user: UserProfile | undefined;
+  user: UserProfile | undefined
 }
 
 export function Home({ user }: IUser) {
-  if (!user) return null;
+  if (!user) return null
 
   return (
     <div>
       Welcome {user.nickname}! <a href="/api/auth/logout">Logout</a>
     </div>
-  );
+  )
 }
