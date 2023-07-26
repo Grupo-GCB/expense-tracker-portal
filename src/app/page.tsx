5ffed96a3aeadf8c28249a4c2703122a9a1fa031
AugustoCVS <guty.vertolis@gmail.com>
2023-07-26 00:09:37 -0300

    feat: creating auth0 login flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import LandingPage from '@/components/LandingPage'
+
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <LandingPage />
     </>
   )
 }
---
+++ src/components/LandingPage/index.tsx
@@ -0,0 +1,10 @@
+export default function LandingPage() {
+  return (
+    <>
+      <h1>Expense Tracker</h1>
+      <button>
+        <a href="/api/auth/login">Login</a>
+      </button>
+    </>
+  )
+}
