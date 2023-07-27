8b9ee6a613b4253d08513f72e17944d3e913c527
charlenefialho <fialhocharlene@gmail.com>
2023-07-27 15:20:14 -0300

    test: adds viewport laptopLarge on storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,16 @@
 import type { Preview } from "@storybook/react";
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
 
-import '../src/styles/tailwind.css';
+import "../src/styles/tailwind.css";
+
+export const laptopLarge = {
+  name: "laptopL",
+  styles: {
+    width: "1440px",
+    height: "871px",
+  },
+  type: "desktop",
+};
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +20,9 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    viewport: {
+      viewports: { ...MINIMAL_VIEWPORTS, laptopLarge },
     },
   },
 };
