044220c26fb9cb416cd7f0e388c5820345a66fa2
AugustoCVS <guty.vertolis@gmail.com>
2023-07-27 19:51:57 -0300

    feat: getting user accessToken

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,24 @@
 import { UserProfile } from '@auth0/nextjs-auth0/client'
+import { useCallback, useEffect } from 'react'
+import axios from 'axios'
 
 interface IUser {
   user: UserProfile | undefined
 }
 
 export function Home({ user }: IUser) {
+  const handleAccessToken = useCallback(async () => {
+    const res = await axios.get<{ accessToken: string }>('api/accessTokenAuth')
+    console.log(res.data.accessToken)
+  }, [])
+
+  useEffect(() => {
+    handleAccessToken()
+  }, [])
+
   if (!user) return null
+
+  console.log(user)
 
   return (
     <div>
