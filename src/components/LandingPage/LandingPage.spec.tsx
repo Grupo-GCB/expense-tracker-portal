5694d88f245d8a888b0649c87ec929f70ef7f869
charlenefialho <fialhocharlene@gmail.com>
2023-07-28 14:00:57 -0300

    test: adds unit test button and link

--- src/components/LandingPage/LandingPage.spec.tsx
+++ src/components/LandingPage/LandingPage.spec.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { LandingPage } from ".";
 
-describe("Header", () => {
+describe("LandingPage", () => {
   it("should be able to render correctly", () => {
     render(<LandingPage />);
     const logo = screen.getByAltText("logo gcb");
@@ -11,16 +11,27 @@ describe("Header", () => {
     const spanHeader = screen.getByText("Expensive Tracker");
     expect(spanHeader).toBeInTheDocument();
 
-    const title = screen.getByText("Trilhe o caminho da estabilidade financeira")
+    const title = screen.getByText(
+      "Trilhe o caminho da estabilidade financeira"
+    );
     expect(title).toBeInTheDocument();
 
-    const button = screen.getByRole('button',{ name: "Experimentar!" })
+    const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
 
     const image = screen.getByAltText("home do site");
     expect(image).toBeInTheDocument();
+  });
 
+  it("should be able to render button text correctly", () => {
+    render(<LandingPage />);
+    const buttonText = screen.getByRole("button", { name: "Experimentar!" });
+    expect(buttonText).toBeInTheDocument();
+  });
 
-    
+  it("should be able to render button link with correct URL", () => {
+    render(<LandingPage />);
+    const linkButton = screen.getByRole("link", { name: "Experimentar!" });
+    expect(linkButton).toHaveAttribute("href", "/api/auth/login");
   });
 });
