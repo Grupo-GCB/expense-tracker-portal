e27f18429ec71282cd9431108e54da7e4ce49591
AugustoCVS <guty.vertolis@gmail.com>
2023-07-28 19:12:26 -0300

    fix: change some of IUser props to optional

--- src/interfaces/User.tsx
+++ src/interfaces/User.tsx
@@ -2,7 +2,7 @@ import { UserProfile } from '@auth0/nextjs-auth0/client'
 
 export interface IUser {
   user: UserProfile | undefined
-  name: string
-  given_name: string
-  family_name: string
+  name?: string
+  given_name?: string
+  family_name?: string
 }
