17056860dc8140086c2ec690b31f43a6b08e8a30
AugustoCVS <guty.vertolis@gmail.com>
2023-07-28 00:05:37 -0300

    feat: try and catch in sessionAuth

--- pages/api/sessionAuth.ts
+++ pages/api/sessionAuth.ts
@@ -2,8 +2,15 @@ import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 async function sessionAuth(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getSession(req, res)
-  res.status(200).json({ session })
+  try {
+    const userSession = await getSession(req, res)
+    res.status(200).json({ userSession })
+  } catch (error: any) {
+    console.log('Error obtaining the session:', error)
+    res
+      .status(error.status || 500)
+      .json({ error: error.message || 'Something went wrong' })
+  }
 }
 
 export default withApiAuthRequired(sessionAuth)
