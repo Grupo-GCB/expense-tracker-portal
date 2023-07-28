64b2f562640d6f511625bb6730f32748a715ab38
charlenefialho <fialhocharlene@gmail.com>
2023-07-28 08:59:55 -0300

    test: adds story of button

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,46 @@
+import { StoryObj, Meta } from "@storybook/react";
+import { Button, IButtonProps } from "./index";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+} as Meta<IButtonProps>;
+
+export const Hover: StoryObj<IButtonProps> = {
+  args: {
+    className:
+      "items-self-center rounded-md bg-green-300 py-3  px-5 text-white font-bold",
+    children: "Experimentar!",
+  },
+};
+
+export const Small: StoryObj<IButtonProps> = {
+  args: {
+    className:
+      "items-self-center rounded-md bg-green-500 py-2  px-4 text-white font-bold text-sm ",
+    children: "Experimentar!",
+  },
+};
+
+export const Medium: StoryObj<IButtonProps> = {
+  args: {
+    className:
+      "items-self-center rounded-md bg-green-500 py-3  px-5 text-white font-bold",
+    children: "Experimentar!",
+  },
+};
+
+export const Big: StoryObj<IButtonProps> = {
+  args: {
+    className:
+      "items-self-center rounded-md bg-green-500 py-4  px-8 text-xl text-white font-bold",
+    children: "Experimentar!",
+  },
+};
+
+export const Link: StoryObj<IButtonProps> = {
+  args: {
+    className: "items-self-center rounded-md bg-green-500 py-3 px-5 text-white font-bold",
+    children: <a className="text-sm">Experimentar!</a>,
+  },
+};
