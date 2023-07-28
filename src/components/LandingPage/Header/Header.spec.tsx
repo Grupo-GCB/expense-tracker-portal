35d37acef3f5b5e2da058cdb4d040b797d8f5588
charlenefialho <fialhocharlene@gmail.com>
2023-07-28 09:58:44 -0300

    test: adds header unit test

---
+++ src/components/LandingPage/Header/Header.spec.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Header } from ".";
+
+describe("Header", () => {
+  it("should be able to render correctly", () => {
+    render(<Header />);
+
+    const image = screen.getByAltText("logo gcb");
+    expect(image).toBeInTheDocument();
+
+    const span = screen.getByText("Expensive Tracker");
+    expect(span).toBeInTheDocument();
+  });
+});
