4dc98948d5df3e454224bc81b45f0ef04b353c36
charlenefialho <fialhocharlene@gmail.com>
2023-07-28 14:44:54 -0300

    test: adds button unit test

---
+++ src/components/Button/Button.spec.tsx
@@ -0,0 +1,29 @@
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+
+import { Button } from "./index";
+
+describe("Button", () => {
+  it("should be able to render the children correctly", () => {
+    const buttonText = "Clique aqui";
+
+    render(<Button>{buttonText}</Button>);
+
+    const button = screen.getByRole("button", { name: buttonText });
+
+    expect(button).toBeInTheDocument();
+  });
+
+  afterEach(cleanup);
+
+  it("should be able to call onClick function when the button is clicked", () => {
+    const handleClick = jest.fn();
+
+    render(<Button onClick={handleClick}>Clique aqui</Button>);
+
+    const button = screen.getByRole("button", { name: "Clique aqui" });
+
+    fireEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
