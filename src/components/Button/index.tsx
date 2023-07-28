94e6887242de09712fa3768a3be6b2694cb8332d
charlenefialho <fialhocharlene@gmail.com>
2023-07-28 14:43:51 -0300

    refactor: adds onClick props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,12 +3,13 @@ import { twMerge } from "tailwind-merge";
 
 export interface IButtonProps extends HTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  onClick?: ()=> void;
 
 }
 
-export function Button({ children, className }: IButtonProps) {
+export function Button({ children, className, onClick }: IButtonProps) {
   return (
-    <button className={twMerge("", className)}>
+    <button className={twMerge("", className)} onClick={onClick}>
       {children}
     </button>
   );
