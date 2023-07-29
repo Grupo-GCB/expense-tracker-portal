1ecd84e11225dfab496fb369352097e9d65aee96
AugustoCVS <guty.vertolis@gmail.com>
2023-07-28 23:25:36 -0300

    refactor: change import global.css to tailwind.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
 
-import '@/styles/globals.css'
+import '@/styles/tailwind.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
