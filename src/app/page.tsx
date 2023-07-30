5e2e3960bb93bf8cbda1ee91aee63053866e9385
AugustoCVS <guty.vertolis@gmail.com>
2023-07-30 16:47:00 -0300

    feat: add loading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@
 import { useUser } from '@auth0/nextjs-auth0/client'
 
 import { Home, LandingPage } from '@/components'
+import { Loading } from '@/components/Loading'
 
 export default function Page() {
   const { user, error, isLoading } = useUser()
 
-  if (isLoading) return <div>Carregando...</div>
+  if (isLoading) return <Loading />
 
   if (error) return <div>{error.message}</div>
 
