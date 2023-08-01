77318a17b2cd0fc0cd668bd32e42aac046089d69
AugustoCVS <guty.vertolis@gmail.com>
2023-08-01 17:39:28 -0300

    docs: create Loading storybooks

---
+++ src/components/Loading/Loading.stories.tsx
@@ -0,0 +1,12 @@
+import { StoryObj } from '@storybook/react'
+
+import { Loading } from '@/components/Loading'
+
+const meta = {
+  title: 'Components/Loading',
+  component: Loading,
+}
+
+export default meta
+
+export const Default: StoryObj = {}
