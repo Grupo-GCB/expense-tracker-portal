74fd694663c33dde017bdb41b77bd9c22df8a7aa
charlenefialho <fialhocharlene@gmail.com>
2023-08-01 14:36:44 -0300

    docs: remove size add className

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import { StoryObj, Meta } from "@storybook/react";
 
-import { Button, IButtonProps } from "./";
+import { Button, IButtonProps } from "@/components/Button";
 
 const meta: Meta<IButtonProps> = {
   title: "Components/Button",
@@ -16,28 +16,28 @@ export default meta;
 
 export const Default: StoryObj<IButtonProps> = {
   args: {
+    className: "py-3 px-5",
     children: "Experimentar!",
-    size: "medium",
   },
 };
 
 export const Small: StoryObj<IButtonProps> = {
   args: {
+    className: "py-2 px-4 text-sm",
     children: "Experimentar!",
-    size: "small",
   },
 };
 
 export const Medium: StoryObj<IButtonProps> = {
   args: {
+    className: "py-3 px-5",
     children: "Experimentar!",
-    size: "medium",
   },
 };
 
 export const Large: StoryObj<IButtonProps> = {
   args: {
+    className: "py-4 px-8 text-xl",
     children: "Experimentar!",
-    size: "large",
   },
 };
