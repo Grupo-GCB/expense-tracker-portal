f2ad9c5a5b105b4146365b1a920dd7c2583d8ca5
AugustoCVS <guty.vertolis@gmail.com>
2023-08-02 10:49:37 -0300

    feat: create getUserSession

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,27 +2,49 @@ import { useCallback, useEffect } from 'react'
 import axios from 'axios'
 
 import { ISession, IUser } from '@/interfaces'
+import { ErrorPage } from '@/components'
 
 export function Home({ user }: IUser) {
-  const handleUserSession = useCallback(async () => {
+  const getUserSession = async (): Promise<ISession | undefined> => {
     try {
       const res = await axios.get<{ userSession: ISession }>('/api/sessionAuth')
-      const userIdToken = res.data.userSession.idToken
-      const userAccessToken = res.data.userSession.accessToken
+      return res.data.userSession
+    } catch (error) {
+      console.error('Erro desconhecido:  ', error)
+    }
+  }
+
+  const handleUserSession = useCallback(async () => {
+    try {
+      const userSession = await getUserSession()
+
+      if (userSession) {
+        const { idToken, accessToken } = userSession
+      } else {
+        throw new Error('Sessão do usuário não disponível.')
+      }
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        console.error('Axios error:', error.message)
+        console.error('Axios error: ', error.message)
       } else {
-        console.error('Unknown error:', error)
+        console.error('Erro desconhecido: ', error)
       }
     }
   }, [])
 
   useEffect(() => {
     handleUserSession()
-  }, [])
+  }, [handleUserSession])
 
-  if (!user) return null
+  if (!user) {
+    return (
+      <ErrorPage
+        errorMessage={
+          'Erro ao encontrar um usuário! Volte para a página de login e tente novamente!'
+        }
+      />
+    )
+  }
 
   return (
     <div>
