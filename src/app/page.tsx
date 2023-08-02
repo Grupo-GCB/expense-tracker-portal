4d870cedfafcef5cac6b66954f45d85a718d59d8
AugustoCVS <guty.vertolis@gmail.com>
2023-08-02 10:45:57 -0300

    refactor: barrel import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,14 @@
 
 import { useUser } from '@auth0/nextjs-auth0/client'
 
-import { Home, LandingPage } from '@/components'
-import { Loading } from '@/components/Loading'
-import { Error } from '@/components/Error'
+import { Home, LandingPage, Loading, ErrorPage } from '@/components'
 
 export default function Page() {
   const { user, error, isLoading } = useUser()
 
   if (isLoading) return <Loading />
 
-  if (error) return <Error errorMessage={error.message} />
+  if (error) return <ErrorPage errorMessage={error.message} />
 
   if (user) {
     return (
