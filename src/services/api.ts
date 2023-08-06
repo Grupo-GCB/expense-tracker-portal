3955364016154c3bdc3d340def2edc423f4fcaa8
AugustoCVS <guty.vertolis@gmail.com>
2023-08-06 17:48:53 -0300

    feat: create axios api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_URL = 'https://localhost.com/8080/'
+export const BASE_URL = 'http://localhost:8080/'
 
 const api = axios.create({
   baseURL: BASE_URL,
