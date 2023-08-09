61d36d3d11e62a0d646790976b3a109061759315
AugustoCVS <guty.vertolis@gmail.com>
2023-08-09 16:22:51 -0300

    feat: create menu options

--- src/components/MenuSideBar/index.tsx
+++ src/components/MenuSideBar/index.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 import logoGCB from '@/app/assets/svg/logo.svg'
 import logoBull from '@/app/assets/svg/logoBull.svg'
-import { XCircle } from 'phosphor-react'
+import { House, Scroll, Wallet, XCircle } from 'phosphor-react'
 import { IUser } from '@/interfaces'
 
 export function MenuSideBar({ user }: IUser) {
@@ -25,13 +25,45 @@ export function MenuSideBar({ user }: IUser) {
       >
         T
       </div>
-      <div className="flex items-center justify-between">
-        <Image src={open ? logoGCB : logoBull} alt="Logo da GCB." />
-        <XCircle
-          color={'#9E9E9E'}
-          className={` ${open ? 'flex' : 'hidden'} w-8 h-8 cursor-pointer`}
-          onClick={() => setOpen(false)}
-        />
+      <div className="flex flex-col gap-36">
+        <div className="flex items-center justify-between">
+          <Image src={open ? logoGCB : logoBull} alt="Logo da GCB." />
+          <XCircle
+            color={'#9E9E9E'}
+            className={` ${open ? 'flex' : 'hidden'} w-8 h-8 cursor-pointer`}
+            onClick={() => setOpen(false)}
+          />
+        </div>
+        <div>
+          <nav>
+            <ul>
+              <li className="flex items-center h-16 text-gray-100 gap-4 cursor-pointer text-base p-4 hover:bg-gray-900 rounded">
+                <House color="white" className="w-8 h-8" />
+                <span
+                  className={`${!open && 'hidden'} origin-left duration-200`}
+                >
+                  Home
+                </span>
+              </li>
+              <li className="flex items-center h-16 text-gray-100 gap-4 cursor-pointer text-base p-4 hover:bg-gray-900 rounded">
+                <Scroll color="white" className="w-8 h-8" />
+                <span
+                  className={`${!open && 'hidden'} origin-left duration-200`}
+                >
+                  Resumo
+                </span>
+              </li>
+              <li className="flex items-center h-16 text-gray-100 gap-4 cursor-pointer text-base p-4 hover:bg-gray-900 rounded">
+                <Wallet color="white" className="w-8 h-8" />
+                <span
+                  className={`${!open && 'hidden'} origin-left duration-200`}
+                >
+                  Carteiras
+                </span>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   )
