32b6c28cdf6aba661d8ddda4a78f445a78b089e1
charlenefialho <fialhocharlene@gmail.com>
2023-08-09 19:05:15 -0300

    test: added login through facebook account

--- cypress/e2e/1-getting-started/login.cy.ts
+++ cypress/e2e/1-getting-started/login.cy.ts
@@ -13,33 +13,19 @@ context('Login', () => {
       .contains(
         'Welcome back! Please enter your credentials to access your account and continue to organize your finances.',
       )
-    cy.get('label[for=username]').should('be.visible').contains('Email address')
-    cy.get('label[for=password]').should('be.visible').contains('Password')
-    cy.get('input[name=username]').should('be.visible')
-    cy.get('input[name=password]').should('be.visible')
-    cy.get('button[data-action=toggle]').should('be.visible')
-    cy.get('a').should('be.visible').contains('Forgot password?')
-    cy.get('button[name=action]').should('be.visible').contains('Continue')
-    cy.get('p').should('be.visible').contains("Don't have an account?")
-    cy.get('a').should('be.visible').contains('Sign up')
+    cy.get('[data-provider="google"]').should('be.visible')
+    cy.contains('button', 'Continue with Microsoft Account').should(
+      'be.visible',
+    )
+    cy.contains('button', 'Continue with Facebook').should('be.visible')
   })
 
-  it('should be able to login with correct informations', () => {
-    cy.get('input[name=username]').type('teste@gmail.com')
-    cy.get('input[name=password]').type('Terra*12')
-    cy.get('button[data-action-button-primary="true"]').click()
-  })
-
-  it('should not be able to login with incorret informatios', () => {
-    cy.get('input[name=username]').type('teste@teste.com')
-    cy.get('input[name=password]').type('Terra*12')
-    cy.get('button[data-action-button-primary="true"]').click()
-    cy.get('span[data-error-code="wrong-email-credentials"]')
-      .should('be.visible')
-      .contains('Wrong email or password')
-  })
-
-  it('should be able to got to the regiser page', () => {
-    cy.get('a').should('be.visible').contains('Sign up').click()
+  it('should be able log in through Facebook account', () => {
+    cy.contains('button', 'Continue with Facebook').click()
+    cy.origin('https://www.facebook.com/', () => {
+      cy.get('#email').type(Cypress.env('USERNAME'), { log: false })
+      cy.get('#pass').type(Cypress.env('PASSWORD'), { log: false })
+      cy.get('#loginbutton').click()
+    })
   })
 })
