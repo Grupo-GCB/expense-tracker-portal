9316dd68d468e1fa211491f699d79484d7bf309c
AugustoCVS <guty.vertolis@gmail.com>
2023-08-09 15:53:43 -0300

    feat: create menu retractable interaction

--- src/components/MenuSideBar/index.tsx
+++ src/components/MenuSideBar/index.tsx
@@ -1,16 +1,37 @@
-import { IUser } from '@/interfaces'
 import Image from 'next/image'
+import { useState } from 'react'
 
 import logoGCB from '@/app/assets/svg/logo.svg'
 import logoBull from '@/app/assets/svg/logoBull.svg'
 import { XCircle } from 'phosphor-react'
+import { IUser } from '@/interfaces'
 
 export function MenuSideBar({ user }: IUser) {
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className="bg-gray-700 h-screen w-80 px-5 py-10">
+    <div
+      className={` ${
+        open ? 'w-80' : 'w-20'
+      } h-screen bg-gray-700 px-5 py-10 relative duration-300`}
+    >
+      <div
+        className={`${
+          open ? 'hidden' : 'flex'
+        } items-center justify-center bg-gray-700 h-16 w-16 absolute cursor-pointer -right-14 top-9 rounded`}
+        onClick={() => {
+          setOpen(true)
+        }}
+      >
+        T
+      </div>
       <div className="flex items-center justify-between">
-        <Image src={logoGCB} alt="Logo da GCB." />
-        <XCircle color={'#9E9E9E'} className="w-8 h-8" />
+        <Image src={open ? logoGCB : logoBull} alt="Logo da GCB." />
+        <XCircle
+          color={'#9E9E9E'}
+          className={` ${open ? 'flex' : 'hidden'} w-8 h-8 cursor-pointer`}
+          onClick={() => setOpen(false)}
+        />
       </div>
     </div>
   )
