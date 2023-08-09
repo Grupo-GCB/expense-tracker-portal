fad615c895b1f77d3e367df1682e0fbdfed7abb7
AugustoCVS <guty.vertolis@gmail.com>
2023-08-09 08:58:05 -0300

    config: create coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,15 @@
-import nextJest from "next/jest.js";
+import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-  dir: "./",
-});
+  dir: './',
+})
 
 const config = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testEnvironment: "jest-environment-jsdom",
-  preset: "ts-jest",
-};
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
+  collectCoverage: true,
+  coverageReporters: ['lcov', 'text'],
+}
 
-export default createJestConfig(config);
+export default createJestConfig(config)
