9fcfcc5aac6b9a149c45c72a66def01246579d17
AugustoCVS <guty.vertolis@gmail.com>
2023-08-10 09:12:56 -0300

    feat: add Header responsivity

--- src/components/MenuSideBar/components/Header.tsx
+++ src/components/MenuSideBar/components/Header.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 
 import logoBull from '@/app/assets/svg/logoBull.svg'
+import logoGCB from '@/app/assets/svg/logo.svg'
+import { IMenuProps } from '@/interfaces'
 
-export function Header() {
+export function Header({open}: IMenuProps) {
   return (
     <div className="flex items-center justify-center">
-      <Image src={logoBull} alt="Logo da GCB." />
+      <Image src={open ? logoGCB: logoBull} alt="Logo da GCB." />
     </div>
   )
 }
