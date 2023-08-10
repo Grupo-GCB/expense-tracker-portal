d1f18f066df54a4009dc8c345352ba286e037337
AugustoCVS <guty.vertolis@gmail.com>
2023-08-10 14:46:33 -0300

    fix: fix side bar responsivity

--- src/components/MenuSideBar/index.tsx
+++ src/components/MenuSideBar/index.tsx
@@ -6,7 +6,7 @@ import { IUser, IUserProfileProps } from '@/interfaces'
 import { Header, MenuOptions, MenuUser, UserProfile } from './components'
 
 
-export function MenuSideBar({ user }: IUserProfileProps) {
+export function MenuSideBar({ user }: IUserProfileProps ) {
   const [open, setOpen] = useState(false)
 
   const handleDestroyUserToken = (): void => {
@@ -18,11 +18,11 @@ export function MenuSideBar({ user }: IUserProfileProps) {
       <div className='lg:hidden absolute cursor-pointer right-5 top-5'>
         <List color="#323238" className="w-8 h-8" onClick={() => setOpen(!open)}/>
       </div>
-      <div className={`lg:static lg:w-28 sm:fixed ${open ? 'left-0 w-full' : '-left-full'} h-full bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300`}>
+      <div className={`lg:static lg:w-28 sm:absolute ${open ? 'left-0 w-full' : '-left-full'} min-h-screen bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300`}>
         <div className='lg:hidden absolute cursor-pointer right-5 top-5'>
           <X color="white" className="w-8 h-8" onClick={() => setOpen(!open)}/>
         </div>
-        <div className="flex flex-col justify-between h-screen">
+        <div className="flex flex-col justify-between">
           <div className="flex flex-col gap-32">
             <Header open={open}/>
             <MenuOptions open={open}/>
@@ -37,10 +37,10 @@ export function MenuSideBar({ user }: IUserProfileProps) {
         <button
           onClick={handleDestroyUserToken}
           className="flex items-center justify-center"
-        >
-          <a href="/api/auth/logout">
-            <SignOut color="white" className="w-8 h-8" />
-          </a>
+          >
+            <a href="/api/auth/logout">
+              <SignOut color="white" className="w-8 h-8" />
+            </a>
         </button>
       </div>
     </div>
