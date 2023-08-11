5a1088a0f2d034719ff04540104c61f1297816ff
AugustoCVS <guty.vertolis@gmail.com>
2023-08-11 12:30:15 -0300

    refactor: change user do optional

--- src/interfaces/UserProfileProps.tsx
+++ src/interfaces/UserProfileProps.tsx
@@ -2,7 +2,7 @@ import { UserProfile } from "@auth0/nextjs-auth0/client";
 import { IUser } from ".";
 
 export interface IUserProfileProps{
-    user: IUser | UserProfile
+    user?: IUser | UserProfile
     open?: boolean
     picture?: string
 }
