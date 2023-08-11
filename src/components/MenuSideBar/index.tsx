deee6efeb56fce185be1a407cfba5119f428f7d0
AugustoCVS <guty.vertolis@gmail.com>
2023-08-11 20:16:50 -0300

    fix: change conditional do show menu side bar

--- src/components/MenuSideBar/index.tsx
+++ src/components/MenuSideBar/index.tsx
@@ -3,19 +3,22 @@
 import nookies from 'nookies'
 import { List, SignOut, X } from 'phosphor-react'
 import { useState } from 'react'
+import { useUser } from '@auth0/nextjs-auth0/client'
 
 import { Header, MenuOptions, MenuUser, UserProfile } from './components'
 
 
+
 export function MenuSideBar() {
   const [open, setOpen] = useState(false)
+  const {user} = useUser()
 
   const handleDestroyUserToken = (): void => {
     nookies.destroy(null, '@user_token', { path: '/' })
   }
 
   return (
-    <div className={`fixed lg:w-28 ${open ? 'sm:left-0 sm:w-full sm:px-12' : 'sm:-left-full lg:left-0'} min-h-screen max-h-screen bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300 overflow-y-scroll scrollbar-thin scrollbar-zinc-950 scrollbar-thumb-800`}>
+    <div className={`${user ? 'flxe' : 'hidden'} fixed lg:w-28 ${open ? 'sm:left-0 sm:w-full sm:px-12' : 'sm:-left-full lg:left-0'} min-h-screen max-h-screen bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300 overflow-y-scroll scrollbar-thin scrollbar-zinc-950 scrollbar-thumb-800`}>
          <div className={`${open ? 'hidden' : 'flex lg:hidden'} fixed cursor-pointer right-5 top-5`}>
           <List color="#f2f2f2" className="w-8 h-8" onClick={() => setOpen(!open)}/>
         </div>
