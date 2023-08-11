f274b0bd0e36a97e5693254e7ba13e7d3dedfe9d
AugustoCVS <guty.vertolis@gmail.com>
2023-08-10 22:00:43 -0300

    feat: change position to fixed

--- src/components/MenuSideBar/index.tsx
+++ src/components/MenuSideBar/index.tsx
@@ -15,30 +15,27 @@ export function MenuSideBar({ user }: IUserProfileProps ) {
 
   return (
     <div>
-      <div className='lg:hidden absolute cursor-pointer right-5 top-5'>
+      <div className='lg:hidden fixed cursor-pointer right-5 top-5'>
         <List color="#323238" className="w-8 h-8" onClick={() => setOpen(!open)}/>
       </div>
-      <div className={`lg:static lg:w-28 sm:absolute ${open ? 'left-0 w-full' : '-left-full'} min-h-screen bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300`}>
-        <div className='lg:hidden absolute cursor-pointer right-5 top-5'>
+      <div className={`fixed lg:w-28 ${open ? 'sm:left-0 sm:w-full sm:px-12' : 'sm:-left-full lg:left-0'} min-h-screen bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300`}>
+        <div className={`${open ? 'flex' : 'hidden'} absolute cursor-pointer right-5 top-5`}>
           <X color="white" className="w-8 h-8" onClick={() => setOpen(!open)}/>
         </div>
-        <div className="flex flex-col justify-between">
+        <div className="flex flex-col justify-between gap-8">
           <div className="flex flex-col gap-32">
             <Header open={open}/>
             <MenuOptions open={open}/>
           </div>
 
-          <div className="flex flex-col items-center gap-8">
+          <div className="flex flex-col gap-8">
             <MenuUser open={open}/>
             <UserProfile user={user} open={open}/>
           </div>
         </div>
 
-        <button
-          onClick={handleDestroyUserToken}
-          className="flex items-center justify-center"
-          >
-            <a href="/api/auth/logout">
+        <button onClick={handleDestroyUserToken}>
+            <a href="/api/auth/logout" className={`flex items-center ${open ? 'justify-start': 'justify-center'}`}>
               <SignOut color="white" className="w-8 h-8" />
             </a>
         </button>
