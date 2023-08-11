151ff67fe3b06fbe3eb969e3f2b26eb4343ec1ce
AugustoCVS <guty.vertolis@gmail.com>
2023-08-10 21:58:04 -0300

    fix: fix component width

--- src/components/MenuSideBar/components/MenuUser.tsx
+++ src/components/MenuSideBar/components/MenuUser.tsx
@@ -6,13 +6,13 @@ export function MenuUser({open}: IMenuProps) {
   return (
     <nav>
       <ul>
-        <li className={`flex ${open ? 'flex-row text-2xl gap-4' : 'flex-col text-sm justify-center'} p-4 items-center h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}>
+        <li className={`flex ${open ? 'flex-row text-2xl gap-4 w-56' : 'flex-col text-sm justify-center'} p-4 items-center h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}>
           <Bell color="white" className="w-8 h-8" />
           {open &&(
             <span>Notificações</span>
           )}
         </li>
-        <li className={`flex ${open ? 'flex-row text-2xl gap-4' : 'flex-col text-sm justify-center'} p-4 items-center h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}>
+        <li className={`flex ${open ? 'flex-row text-2xl gap-4 w-60' : 'flex-col text-sm justify-center'} p-4 items-center h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}>
           <Gear color="white" className="w-8 h-8" />
           {open &&(
             <span>Configurações</span>
