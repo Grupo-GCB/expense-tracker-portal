c11966ab5a4b3235a13f993bc89743cc18cd673b
AugustoCVS <guty.vertolis@gmail.com>
2023-08-11 16:44:18 -0300

    fix: fix List icon position

--- src/components/MenuSideBar/index.tsx
+++ src/components/MenuSideBar/index.tsx
@@ -16,7 +16,7 @@ export function MenuSideBar() {
 
   return (
     <div className={`fixed lg:w-28 ${open ? 'sm:left-0 sm:w-full sm:px-12' : 'sm:-left-full lg:left-0'} min-h-screen max-h-screen bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300 overflow-y-scroll scrollbar-thin scrollbar-zinc-950 scrollbar-thumb-800`}>
-         <div className='lg:hidden fixed cursor-pointer right-5 top-5'>
+         <div className={`${open ? 'hidden' : 'flex lg:hidden'} fixed cursor-pointer right-5 top-5`}>
           <List color="#f2f2f2" className="w-8 h-8" onClick={() => setOpen(!open)}/>
         </div>
 
