14fc34261917b752cb3c3950c3fbfd9b3ebe9aca
AugustoCVS <guty.vertolis@gmail.com>
2023-08-11 20:00:47 -0300

    docs: create MenuUser storybook

---
+++ src/components/MenuSideBar/components/MenuUser/MenuUser.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { MenuUser } from '.'
+import { IMenuProps } from '@/interfaces'
+
+const meta: Meta <IMenuProps> = {
+    title: 'MenuSideBar/Components/MenuUser',
+    component: MenuUser,
+    argTypes: {
+        open:{
+            defaultValue: false,
+            type: 'boolean'
+        }
+    },
+    parameters: {
+        layout: 'centered', 
+        viewport: {
+            defaultViewport: 'iphone5',
+        },
+    },
+  };
+
+export default meta
+
+export const Default: StoryObj<IMenuProps> = {
+    args:{
+        open: false
+    }
+}
