0edea9ee9ec59db16bb74e6434e951a37e5f86d4
AugustoCVS <guty.vertolis@gmail.com>
2023-08-11 08:49:54 -0300

    fix: fix user profile image bug

--- src/components/MenuSideBar/components/UserProfile.tsx
+++ src/components/MenuSideBar/components/UserProfile.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image'
 
-import { IMenuProps, IUser, IUserProfileProps } from '@/interfaces'
-
-import logoBull from '@/app/assets/svg/logoBull.svg'
+import { IUserProfileProps } from '@/interfaces'
 
 export function UserProfile({open, user}: IUserProfileProps) {
   return (
     <div className={`flex ${open ? 'flex-row h-20 gap-4 text-2xl w-56': 'flex-col h-32 justify-center w-full'} items-center px-4 py-6 gap-2 bg-gray-900 rounded-xl`}>
-      <Image
-        src={logoBull}
-        alt="Imagem do usuário."
-        className="w-12 h-12 bg-gray-500 rounded-full"
-      />
+      {user.picture && typeof user.picture === 'string' && user.picture.trim() !== '' && (
+        <Image
+          src={user.picture}
+          width={24}
+          height={24}
+          alt="Imagem do usuário."
+          className="w-12 h-12 bg-gray-500 rounded-full"
+        />
+      )}
       <p className="flex text-white text-base">{user?.name}</p>
     </div>
   )
