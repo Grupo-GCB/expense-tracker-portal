a759d5071791567240f6e9925e397ea519cb3bc7
AugustoCVS <guty.vertolis@gmail.com>
2023-08-11 08:51:04 -0300

    refactor: delete useless div

--- src/components/MenuSideBar/index.tsx
+++ src/components/MenuSideBar/index.tsx
@@ -14,24 +14,23 @@ export function MenuSideBar({ user }: IUserProfileProps ) {
   }
 
   return (
-    <div>
+    <div className={`fixed lg:w-28 ${open ? 'sm:left-0 sm:w-full sm:px-12' : 'sm:-left-full lg:left-0'} min-h-screen bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300`}>
       <div className='lg:hidden fixed cursor-pointer right-5 top-5'>
         <List color="#323238" className="w-8 h-8" onClick={() => setOpen(!open)}/>
       </div>
-      <div className={`fixed lg:w-28 ${open ? 'sm:left-0 sm:w-full sm:px-12' : 'sm:-left-full lg:left-0'} min-h-screen bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300`}>
-        <div className={`${open ? 'flex' : 'hidden'} absolute cursor-pointer right-5 top-5`}>
-          <X color="white" className="w-8 h-8" onClick={() => setOpen(!open)}/>
+      <div className={`${open ? 'flex' : 'hidden'} absolute cursor-pointer right-5 top-5`}>
+        <X color="white" className="w-8 h-8" onClick={() => setOpen(!open)}/>
+      </div>
+      <div className="flex flex-col justify-between gap-8">
+        <div className="flex flex-col gap-32">
+          <Header open={open}/>
+          <MenuOptions open={open}/>
         </div>
-        <div className="flex flex-col justify-between gap-8">
-          <div className="flex flex-col gap-32">
-            <Header open={open}/>
-            <MenuOptions open={open}/>
-          </div>
 
-          <div className="flex flex-col gap-8">
-            <MenuUser open={open}/>
-            <UserProfile user={user} open={open}/>
-          </div>
+        <div className="flex flex-col gap-8">
+          <MenuUser open={open}/>
+          <UserProfile user={user} open={open}/>
+        </div>
         </div>
 
         <button onClick={handleDestroyUserToken}>
@@ -39,7 +38,6 @@ export function MenuSideBar({ user }: IUserProfileProps ) {
               <SignOut color="white" className="w-8 h-8" />
             </a>
         </button>
-      </div>
     </div>
   )
 }
