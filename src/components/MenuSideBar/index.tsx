78b61eb7b418539940e2aef43599cf59453e26d1
AugustoCVS <guty.vertolis@gmail.com>
2023-08-11 12:28:26 -0300

    feat: add scroll in the sidebar

--- src/components/MenuSideBar/index.tsx
+++ src/components/MenuSideBar/index.tsx
@@ -1,12 +1,13 @@
+'use client'
+
 import nookies from 'nookies'
 import { List, SignOut, X } from 'phosphor-react'
 import { useState } from 'react'
 
-import { IUserProfileProps } from '@/interfaces'
 import { Header, MenuOptions, MenuUser, UserProfile } from './components'
 
 
-export function MenuSideBar({ user }: IUserProfileProps ) {
+export function MenuSideBar() {
   const [open, setOpen] = useState(false)
 
   const handleDestroyUserToken = (): void => {
@@ -14,23 +15,26 @@ export function MenuSideBar({ user }: IUserProfileProps ) {
   }
 
   return (
-    <div className={`fixed lg:w-28 ${open ? 'sm:left-0 sm:w-full sm:px-12' : 'sm:-left-full lg:left-0'} min-h-screen bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300`}>
-      <div className='lg:hidden fixed cursor-pointer right-5 top-5'>
-        <List color="#323238" className="w-8 h-8" onClick={() => setOpen(!open)}/>
-      </div>
-      <div className={`${open ? 'flex' : 'hidden'} absolute cursor-pointer right-5 top-5`}>
-        <X color="white" className="w-8 h-8" onClick={() => setOpen(!open)}/>
-      </div>
-      <div className="flex flex-col justify-between gap-8">
-        <div className="flex flex-col gap-32">
-          <Header open={open}/>
-          <MenuOptions open={open}/>
+    <div className={`fixed lg:w-28 ${open ? 'sm:left-0 sm:w-full sm:px-12' : 'sm:-left-full lg:left-0'} min-h-screen max-h-screen bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300 overflow-y-scroll scrollbar-thin scrollbar-zinc-950 scrollbar-thumb-800`}>
+         <div className='lg:hidden fixed cursor-pointer right-5 top-5'>
+          <List color="#f2f2f2" className="w-8 h-8" onClick={() => setOpen(!open)}/>
         </div>
 
-        <div className="flex flex-col gap-8">
-          <MenuUser open={open}/>
-          <UserProfile user={user} open={open}/>
+        <div className={`${open ? 'flex' : 'hidden'} absolute cursor-pointer right-5 top-5`}>
+          <X color="white" className="w-8 h-8" onClick={() => setOpen(!open)}/>
         </div>
+
+        <div className="flex flex-col justify-between gap-8">
+          <div className="flex flex-col gap-32">
+            <Header open={open}/>
+            <MenuOptions open={open}/>
+          </div>
+
+          <div className="flex flex-col gap-8">
+            <MenuUser open={open}/>
+            <UserProfile open={open}/>
+          </div>
+          
         </div>
 
         <button onClick={handleDestroyUserToken}>
