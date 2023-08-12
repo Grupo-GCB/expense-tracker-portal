58686fcf487a5dbd804afe8b973da43d8888e491
AugustoCVS <guty.vertolis@gmail.com>
2023-08-12 03:09:38 -0300

    feat: handle menu resize

--- src/components/MenuSideBar/index.tsx
+++ src/components/MenuSideBar/index.tsx
@@ -2,11 +2,10 @@
 
 import nookies from 'nookies'
 import { List, SignOut, X } from 'phosphor-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useUser } from '@auth0/nextjs-auth0/client'
 
 import { Header, MenuOptions, MenuUser, UserProfile } from './components'
-
 
 
 export function MenuSideBar() {
@@ -16,6 +15,21 @@ export function MenuSideBar() {
   const handleDestroyUserToken = (): void => {
     nookies.destroy(null, '@user_token', { path: '/' })
   }
+
+  const handleMenuResize = (): void => {
+    if (window.innerWidth >= 1224) {
+      setOpen(false);
+    }
+  }
+
+  useEffect(() => {
+    handleMenuResize()
+
+    window.addEventListener('resize', handleMenuResize);
+    return () => {
+      window.removeEventListener('resize', handleMenuResize);
+    };
+  }, [handleMenuResize]);
 
   return (
     <div className={`${user ? 'flex' : 'hidden'} fixed lg:w-28 ${open ? 'sm:left-0 sm:w-full sm:px-12' : 'sm:-left-full lg:left-0'} min-h-screen max-h-screen bg-gray-700 px-5 py-10 top-0 flex flex-col gap-16 duration-300 overflow-y-scroll scrollbar-thin scrollbar-zinc-950 scrollbar-thumb-800`} data-testid='menuNav'>
