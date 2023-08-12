35c33eb923e276ce7d1649493c834ecb261193bb
AugustoCVS <guty.vertolis@gmail.com>
2023-08-11 23:10:53 -0300

    feat: add data-set in icons

--- src/components/MenuSideBar/components/MenuUser/index.tsx
+++ src/components/MenuSideBar/components/MenuUser/index.tsx
@@ -7,13 +7,13 @@ export function MenuUser({open}: IMenuProps) {
     <nav>
       <ul>
         <li className={`flex ${open ? 'flex-row text-2xl gap-4 w-56' : 'flex-col text-sm justify-center'} p-4 items-center h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}>
-          <Bell color="white" className="w-8 h-8" />
+          <Bell color="white" className="w-8 h-8" data-testid='notificacoesIcon'/>
           {open &&(
             <span>Notificações</span>
           )}
         </li>
         <li className={`flex ${open ? 'flex-row text-2xl gap-4 w-60' : 'flex-col text-sm justify-center'} p-4 items-center h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}>
-          <Gear color="white" className="w-8 h-8" />
+          <Gear color="white" className="w-8 h-8" data-testid='configuracoesIcon'/>
           {open &&(
             <span>Configurações</span>
           )}
