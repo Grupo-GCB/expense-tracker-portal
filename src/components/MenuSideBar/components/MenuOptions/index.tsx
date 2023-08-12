b4275edeca89501cc2d06b60fee6a902bd65ad72
AugustoCVS <guty.vertolis@gmail.com>
2023-08-11 22:56:37 -0300

    feat: add data-set in icons

--- src/components/MenuSideBar/components/MenuOptions/index.tsx
+++ src/components/MenuSideBar/components/MenuOptions/index.tsx
@@ -7,15 +7,15 @@ export function MenuOptions({open}: IMenuProps) {
     <nav>
       <ul className='flex flex-col gap-8'>
         <li className={`flex ${open ? 'flex-row text-2xl gap-4 p-4 w-48' : 'flex-col text-sm justify-center'} items-center h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}>
-          <House color="white" className="w-8 h-8" />
+          <House color="white" className="w-8 h-8" data-testid='homeIcon' />
           <span>Home</span>
         </li>
         <li className={`flex ${open ? 'flex-row text-2xl gap-4 p-4 w-48' : 'flex-col text-sm justify-center'} items-center  h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}>
-          <Scroll color="white" className="w-8 h-8" />
+          <Scroll color="white" className="w-8 h-8" data-testid='resumoIcon'/>
           <span>Resumo</span>
         </li>
         <li className={`flex ${open ? 'flex-row text-2xl gap-4 p-4 w-48' : 'flex-col text-sm justify-center'} items-center  h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}>
-          <Wallet color="white" className="w-8 h-8" />
+          <Wallet color="white" className="w-8 h-8" data-testid='carteirasIcon'/>
           <span>Carteiras</span>
         </li>
       </ul>
