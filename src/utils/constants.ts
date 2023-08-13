c0e4a170f26f0010541af040c3b1c7db825a7ba2
charlenefialho <fialhocharlene@gmail.com>
2023-08-13 20:27:10 -0300

    refactor: accountType

--- src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = 'http://localhost:8080/'
-export const DEFAULT_UNKNOWN_ERROR_MESSAGE = 'Algo deu errado!'
-export const UNKNOWN_ERROR = 'Erro desconhecido: '
-export const AXIOS_ERROR = 'Axios error: '
-export const THIRTY_DAY_COOKIE_LIFETIME = 30 * 24 * 60 * 60
