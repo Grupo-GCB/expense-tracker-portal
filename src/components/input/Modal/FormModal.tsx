24e24c51c5254c15ac7aa7873ee9a959ad60d5fa
charlenefialho <fialhocharlene@gmail.com>
2023-08-13 20:19:12 -0300

    feat: custom select

---
+++ src/components/input/Modal/FormModal.tsx
@@ -0,0 +1,17 @@
+import { HTMLAttributes } from 'react'
+
+interface IFormModal extends HTMLAttributes<HTMLFormElement> {
+  action?: (formData: FormData) => void;
+}
+
+export function FormModal({ children, action, onSubmit }: IFormModal) {
+  return (
+    <form
+      action={action}
+      className="flex flex-col gap-4 mt-5 md:mt-10 w-full"
+      onSubmit={onSubmit}
+    >
+      {children}
+    </form>
+  );
+}
