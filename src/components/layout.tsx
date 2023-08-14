bf757209f9e24e0f8a7d86701a2e5711476db7b3
charlenefialho <fialhocharlene@gmail.com>
2023-08-14 12:09:52 -0300

    refactor: layout styles

---
+++ src/components/layout.tsx
@@ -0,0 +1,26 @@
+import { UserProvider } from '@auth0/nextjs-auth0/client'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+import '@/styles/tailwind.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Expense Tracker',
+  description: 'Expense tracker portal',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <UserProvider>
+        <body className={inter.className}>{children}</body>
+      </UserProvider>
+    </html>
+  )
+}
