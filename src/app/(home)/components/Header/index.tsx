d735782019c791cc7a7138c37a6192e6ca95dcb1
charlenefialho <fialhocharlene@gmail.com>
2023-08-15 14:39:54 -0300

    refactor: directory router home

---
+++ src/app/(home)/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+import logo from '@/app/assets/svg/logo.svg'
+
+export function Header() {
+  return (
+    <header className="h-30 font-roboto font-semibold text-white flex justify-center bg-gray-300">
+      <nav className="p-5">
+        <div className="sm:flex sm:flex-col  md:flex md:flex-row md:text-4xl">
+          <Image className="h-11" src={logo} alt="Logo do Grupo GCB" />
+          <span className="h-11 flex items-center justify-center ">
+            Expensive Tracker
+          </span>
+        </div>
+      </nav>
+    </header>
+  )
+}
