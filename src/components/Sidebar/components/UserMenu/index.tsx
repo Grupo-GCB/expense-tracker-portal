202b22ea8daf2e3be19b18a5187818d04caf16de
AugustoCVS <guty.vertolis@gmail.com>
2023-08-15 19:25:42 -0300

    feat: create a map for the user menu options

--- src/components/Sidebar/components/UserMenu/index.tsx
+++ src/components/Sidebar/components/UserMenu/index.tsx
@@ -3,37 +3,49 @@ import { Bell, Gear } from "phosphor-react";
 import { IMenu } from "@/interfaces";
 
 export function UserMenu({ open }: IMenu) {
+  const options = [
+    {
+      id: 1,
+      icon: (
+        <Bell
+          color="white"
+          className="w-8 h-8"
+          data-testid="notificationsIcon"
+        />
+      ),
+      title: "Notificações",
+    },
+    {
+      id: 1,
+      icon: (
+        <Gear
+          color="white"
+          className="w-8 h-8"
+          data-testid="configurationsIcon"
+        />
+      ),
+      title: "Configurações",
+    },
+  ];
+
   return (
     <nav>
       <ul>
-        <li
-          className={`flex ${
-            open
-              ? "flex-row text-2xl gap-4 w-56"
-              : "flex-col text-sm justify-center"
-          } p-4 items-center h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}
-        >
-          <Bell
-            color="white"
-            className="w-8 h-8"
-            data-testid="notificationsIcon"
-          />
-          {open && <span>Notificações</span>}
-        </li>
-        <li
-          className={`flex ${
-            open
-              ? "flex-row text-2xl gap-4 w-60"
-              : "flex-col text-sm justify-center"
-          } p-4 items-center h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}
-        >
-          <Gear
-            color="white"
-            className="w-8 h-8"
-            data-testid="configurationsIcon"
-          />
-          {open && <span>Configurações</span>}
-        </li>
+        {options.map((option) => {
+          return (
+            <li
+              key={option.id}
+              className={`flex ${
+                open
+                  ? "flex-row text-2xl gap-4 w-60"
+                  : "flex-col text-sm justify-center"
+              } p-4 items-center h-16 text-gray-100 cursor-pointer hover:bg-gray-900 rounded`}
+            >
+              {option.icon}
+              {open && <span>{option.title}</span>}
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
