8caeca8727a2adb881e7283f4efc2f7b429ac109
charlenefialho <fialhocharlene@gmail.com>
2023-08-15 14:37:04 -0300

    refactor: adde register-wallet to folder component

---
+++ src/app/carteira/components/RegisterWallet/register-wallet.component.tsx
@@ -0,0 +1,57 @@
+import React  from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
+import { CircleNotch } from 'phosphor-react'
+
+import { Button } from '@/components/Button'
+import { FormModal } from '@/components/Modal/FormModal'
+import { CustomSelect } from '@/components/Select'
+import { accountTypes } from '@/utils/constants'
+
+import { IRegisterWallet } from "../../types" 
+import { useRegisterWallet } from './register-wallet.hook'
+
+export function RegisterWallet({ setOpen }: IRegisterWallet) {
+  const { actions,states } = useRegisterWallet({setOpen})
+  
+  return ( 
+    <FormModal action={actions.test}>
+      <CustomSelect options={accountTypes} placeholder="Tipo da conta" name="account_type" testid="input-type-account"/>
+      <CustomSelect options={states.bankList} placeholder="Banco" name="bank_id" testid="input-bank"/>
+      <textarea
+        name="description"
+        id="description"
+        cols={30}
+        rows={3}
+        maxLength={32}
+        placeholder="Descrição"
+        className="mb-3 md:mb-10 bg-gray-900 pl-4 pt-4 rounded ou focus:outline-none focus:ring focus:ring-green-300 resize-none"
+      ></textarea>
+      <div className="flex flex-col md:flex-row gap-2 md:gap-4 md:w-full md:justify-center">
+        <Button
+          type="submit"
+          className="py-2 px-4 md:py-4 md:px-6 bg-green-500 w-full md:w-full rounded-[6px]"
+          onClick={actions.handleSaveForm}
+          disabled={states.isSavingDataForms}
+          canceled={false}
+        >
+          {states.isSavingDataForms ? (
+            <CircleNotch
+              className="animate-spin w-full justify-center"
+              data-testid="loading-icon"
+            />
+          ) : (
+            <span>Confirmar!</span>
+          )}
+        </Button>
+        <Dialog.Close asChild>
+          <Button
+            className="py-2 px-4 md:py-4 md:px-6 w-full md:w-full rounded-[6px]"
+            canceled
+          >
+            Cancelar
+          </Button>
+        </Dialog.Close>
+      </div>
+    </FormModal>
+  );
+}
