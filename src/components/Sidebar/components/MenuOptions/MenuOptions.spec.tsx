f12373e1577d7eecbb915db3213913892bc80824
AugustoCVS <guty.vertolis@gmail.com>
2023-08-15 16:25:21 -0300

    chore: rename test's titles

--- src/components/Sidebar/components/MenuOptions/MenuOptions.spec.tsx
+++ src/components/Sidebar/components/MenuOptions/MenuOptions.spec.tsx
@@ -4,7 +4,7 @@ import { render, screen } from "@testing-library/react";
 import { MenuOptions } from ".";
 
 describe("MenuOptions", () => {
-  it("it should be able to render the menu options with correct icons and labels", () => {
+  it("should be able to render the menu options with the correct icons and labels", () => {
     render(<MenuOptions open={true} />);
 
     const homeIcon = screen.getByTestId("homeIcon");
@@ -26,7 +26,7 @@ describe("MenuOptions", () => {
     expect(carteirasLabel).toBeInTheDocument();
   });
 
-  it("should be able to render correct style in icons when open prop is true", () => {
+  it("should be able to render the correct styles for icons when the open prop is true", () => {
     render(<MenuOptions open={true} />);
 
     const homeIcon = screen.getByTestId("homeIcon");
@@ -38,7 +38,7 @@ describe("MenuOptions", () => {
     expect(carteirasIcon).toHaveClass("w-8 h-8");
   });
 
-  it("should be able to render correct style in icons when open prop is false", () => {
+  it("should be able to render the correct styles for icons when the open prop is false", () => {
     render(<MenuOptions open={false} />);
 
     const homeIcon = screen.getByTestId("homeIcon");
