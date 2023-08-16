bbebdf84ca5e0e779e261cd43e4839c0c266884d
charlenefialho <fialhocharlene@gmail.com>
2023-08-16 15:25:31 -0300

    refactor: removed unusable barrel export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,10 +3,4 @@ export * from './Loading'
 export * from './Modal'
 export * from './Button'
 export * from './Select'
-
-export * from "./ErrorPage";
-export * from "./Home";
-export * from "./LandingPage";
-export * from "./Loading";
 export * from "./Sidebar";
-export * from "./Button";
