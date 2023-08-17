c503309a59dc198bdb40d383ed44ef30f6b52ff4
charlenefialho <fialhocharlene@gmail.com>
2023-08-17 18:03:38 -0300

    refactor: imports and function typing

--- src/app/carteira/components/RegisterWallet/register-wallet.hook.ts
+++ src/app/carteira/components/RegisterWallet/register-wallet.hook.ts
@@ -1,13 +1,14 @@
-'use client' 
+"use client";
 
-import {  useEffect, useState } from 'react'
-import { toast } from 'react-toastify'
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-import { getBanks, registerWallet } from '@/app/carteira/action'
-import {  IOptions } from '@/components/Select'
-import { IRegisterWallet } from "../../types"  
+import { getBanks, registerWallet } from "@/app/carteira/action";
+import { IRegisterWallet } from "@/app/carteira/types";
+import { IOptions } from "@/components/Select";
+import { IBank } from "@/interfaces";
 
-export const useRegisterWallet = ({ setOpen }: IRegisterWallet) =>{ 
+export const useRegisterWallet = ({ setOpen }: IRegisterWallet) => {
   const [bankList, setBankList] = useState<IOptions[]>([]);
   const [isSavingDataForms, setIsSavingDataForms] = useState<boolean>(false);
 
@@ -22,39 +23,38 @@ export const useRegisterWallet = ({ setOpen }: IRegisterWallet) =>{
   };
 
   useEffect(() => {
-    async function fetchBanks() {
+    async function fetchBanks(): Promise<void> {
       try {
-        const banks = await getBanks();
+        const banks: IBank[] = await getBanks();
         const formattedOptions = banks.map((bank) => ({
           value: bank.id,
           label: bank.name,
         }));
         setBankList(formattedOptions);
-        toast.success('sucesso')
       } catch (error) {
-        toast.error(`Erro ao buscar os bancos:${error}`);
+        toast.error(`Erro ao buscar os bancos: ${error}`);
       }
     }
     fetchBanks();
   }, []);
 
-  const handleRegisterWallet = async (values : FormData) => {
-    try{
-     const reponse : string  = await registerWallet(values)
-     toast.success(reponse)
-    }catch(error: any){
-      toast.error(error)
+  const handleRegisterWallet = async (values: FormData) => {
+    try {
+      const reponse: string = await registerWallet(values);
+      toast.success(reponse);
+    } catch (error) {
+      toast.error(`${error}`);
     }
-  }
+  };
 
   return {
-    states:{
+    states: {
       bankList,
-      isSavingDataForms
+      isSavingDataForms,
     },
-    actions:{
+    actions: {
       handleRegisterWallet,
-      handleSaveForm
-    }
-  }
-}
+      handleSaveForm,
+    },
+  };
+};
