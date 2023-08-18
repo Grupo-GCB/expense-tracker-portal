d0b53b6b134f4d61bcb2d5f2583c115696cf1c85
charlenefialho <fialhocharlene@gmail.com>
2023-08-18 15:58:30 -0300

    test: registerWallet

--- src/app/carteira/components/RegisterWallet/register-wallet.spec.tsx
+++ src/app/carteira/components/RegisterWallet/register-wallet.spec.tsx
@@ -1,18 +1,18 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 
-import {RegisterWallet} from '.'
+import { RegisterWallet } from ".";
 
 describe("RegisterWallet Component", () => {
   it("should be able to render the component correctly", () => {
     const setOpen = jest.fn();
 
-    render(<RegisterWallet setOpen={setOpen} />);  
+    render(<RegisterWallet setOpen={setOpen} />);
 
     const tipoContaInput = screen.getByPlaceholderText("Tipo da conta");
     const bancoInput = screen.getByPlaceholderText("Banco");
     const descricaoTextarea = screen.getByPlaceholderText("Descrição");
-    const confirmButton = screen.getByText("Confirmar");
-    const cancelButton = screen.getByText("Cancelar");
+    const confirmButton = screen.getByRole("button", { name: "Confirmar" });
+    const cancelButton = screen.getByRole("button", { name: "Cancelar" });
 
     expect(tipoContaInput).toBeInTheDocument();
     expect(bancoInput).toBeInTheDocument();
@@ -21,21 +21,21 @@ describe("RegisterWallet Component", () => {
     expect(cancelButton).toBeInTheDocument();
   });
 
-  it("handles form submission", async () => {
+  it("should be able to handle form submission", async () => {
     const mockSetOpen = jest.fn();
     render(<RegisterWallet setOpen={mockSetOpen} />);
 
     const tipoContaInput = screen.getByPlaceholderText("Tipo da conta");
     const bancoInput = screen.getByPlaceholderText("Banco");
     const descricaoTextarea = screen.getByPlaceholderText("Descrição");
-    const confirmButton = screen.getByText("Confirmar!");
+    const confirmButton = screen.getByText("Confirmar");
 
     fireEvent.change(tipoContaInput, { target: { value: "Tipo de Conta" } });
     fireEvent.change(bancoInput, { target: { value: "Santander" } });
     fireEvent.change(descricaoTextarea, {
       target: { value: "Descrição do Wallet" },
     });
-    
+
     fireEvent.click(confirmButton);
 
     await screen.findByTestId("loading-icon");
