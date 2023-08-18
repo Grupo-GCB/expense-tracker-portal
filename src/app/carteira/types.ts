5a2fc575824102f502db37ec9a89a2d8031a1868
charlenefialho <fialhocharlene@gmail.com>
2023-08-18 01:11:03 -0300

    feat: added interface ICardWallet

--- src/app/carteira/types.ts
+++ src/app/carteira/types.ts
@@ -7,3 +7,10 @@ export interface IRegisterWallet {
 export interface DecodedToken {
   sub: string;
 }
+
+export interface ICardWallet{
+  idWallet:string
+  nameBank:string
+  typeAccount:string
+  description:string
+}
