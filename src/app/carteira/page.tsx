372ec7a53fef83b7efd78efb66bb1bf8da54c05d
AugustoCVS <guty.vertolis@gmail.com>
2023-08-18 12:23:37 -0300

    fix: fix the height and the position of the wallet

--- src/app/carteira/page.tsx
+++ src/app/carteira/page.tsx
@@ -25,28 +25,28 @@ const itemsVisibles = {
 
 const infoCard = [
   {
-    id: "9252b22b-9f81-4c76-bae8-f27739fb30b2",
+    id: "c3463f8d-c75f-4210-aed0-1fcb81044dc5",
     account_type: "Dinheiro",
-    description: "blabla",
+    description: "teste",
     created_at: "2023-08-17T22:40:02.184Z",
     updated_at: "2023-08-17T22:40:02.184Z",
     deleted_at: null,
   },
   {
-    "account_type": "Investimento",
-    "description": "Teste",
-    "updated_at": "2023-08-18T07:23:32.537Z",
-    "deleted_at": null,
-    "id": "efd95476-b32e-4597-aedc-903e43bc6b43",
-    "created_at": "2023-08-18T07:23:32.537Z"
+    account_type: "Investimento",
+    description: "Teste",
+    updated_at: "2023-08-18T07:23:32.537Z",
+    deleted_at: null,
+    id: "efd95476-b32e-4597-aedc-903e43bc6b43",
+    created_at: "2023-08-18T07:23:32.537Z",
   },
   {
-    "account_type": "Conta-Corrente",
-    "description": "Minha conta secundáriadfverget",
-    "updated_at": "2023-08-18T07:24:16.284Z",
-    "deleted_at": null,
-    "id": "0e19a67c-416c-4cb1-8323-06e6a0c3f555",
-    "created_at": "2023-08-18T07:24:16.284Z"
+    account_type: "Conta-Corrente",
+    description: "Minha conta secundáriadfverget",
+    updated_at: "2023-08-18T07:24:16.284Z",
+    deleted_at: null,
+    id: "0e19a67c-416c-4cb1-8323-06e6a0c3f555",
+    created_at: "2023-08-18T07:24:16.284Z",
   },
 ];
 
@@ -67,37 +67,39 @@ export function Carteira() {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <section className="bg-gray-800 max-h-fit ">
+    <>
       <header className="w-full h-24 bg-gray-900 flex items-center justify-center lg:pl-28">
         <h1 className="text-white font-semibold text-lg  md:text-xl">
           Carteiras
         </h1>
       </header>
-      <main className="w-full lg:pl-28 ">
-        <section className="w-full mt-6">
-          <AliceCarousel
-            items={createCardItems()}
-            responsive={itemsVisibles}
-            disableButtonsControls
-          />
-        </section>
-        <Modal open={open} onOpenChange={setOpen}>
-          <div className="w-full flex justify-center">
-            <Modal.Button className="rounded py-2 px-10 mb-5" asChild>
-              <Button canceled={false}>cadastrar nova carteira</Button>
-            </Modal.Button>
-          </div>
-          <Modal.Content>
-            <div className="flex justify-start">
-              <Dialog.Title className="text-lg md:text-xl">
-                Registrar Carteira
-              </Dialog.Title>
+      <section className="bg-gray-800 h-screen max-h-fit flex items-center">
+        <main className="w-full lg:pl-28">
+          <section className="w-full py-6">
+            <AliceCarousel
+              items={createCardItems()}
+              responsive={itemsVisibles}
+              disableButtonsControls
+            />
+          </section>
+          <Modal open={open} onOpenChange={setOpen}>
+            <div className="w-full flex justify-center">
+              <Modal.Button className="rounded py-2 px-10 mb-5" asChild>
+                <Button canceled={false}>cadastrar nova carteira</Button>
+              </Modal.Button>
             </div>
-            <RegisterWallet setOpen={setOpen} />
-          </Modal.Content>
-        </Modal>
-      </main>
-    </section>
+            <Modal.Content>
+              <div className="flex justify-start">
+                <Dialog.Title className="text-lg md:text-xl">
+                  Registrar Carteira
+                </Dialog.Title>
+              </div>
+              <RegisterWallet setOpen={setOpen} />
+            </Modal.Content>
+          </Modal>
+        </main>
+      </section>
+    </>
   );
 }
 export default Carteira;
