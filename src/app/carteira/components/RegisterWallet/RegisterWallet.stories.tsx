198c0dad0f5768de7dc15ce60fd1348cd42fcce1
charlenefialho <fialhocharlene@gmail.com>
2023-08-21 11:57:30 -0300

    doc: added registerWallet story

---
+++ src/app/carteira/components/RegisterWallet/RegisterWallet.stories.tsx
@@ -0,0 +1,55 @@
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Modal } from "@/components";
+import RegisterWallet from "./";
+
+export default {
+  title: "Components/RegisterWallet",
+  component: RegisterWallet,
+  decorators: [
+    (Story) => (
+      <div className="w-72">
+        <Modal>{Story()}</Modal>
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: "fullscreen",
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
+  },
+} as Meta;
+
+export const Mobile: StoryObj = {
+  parameters: {
+    viewport: {
+      defaultViewport: "iphone5",
+    },
+  },
+};
+
+export const Tablet: StoryObj = {
+  parameters: {
+    viewport: {
+      defaultViewport: "ipad",
+    },
+  },
+};
+
+export const Laptop: StoryObj = {
+  parameters: {
+    viewport: {
+      defaultViewport: "laptop",
+    },
+  },
+};
+
+export const LaptopLarge: StoryObj = {
+  parameters: {
+    viewport: {
+      defaultViewport: "laptopLarge",
+    },
+  },
+};
