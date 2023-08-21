763504e5386e40afb6e92b42686bab75fbfb374b
charlenefialho <fialhocharlene@gmail.com>
2023-08-21 05:36:11 -0300

    doc: added storie carteira

---
+++ src/app/carteira/Carteira.stories.tsx
@@ -0,0 +1,47 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Carteira } from './page'
+
+export default {
+  title: 'Pages/Carteira',
+  component: Carteira,
+  parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
+  },
+} as Meta
+
+export const Mobile: StoryObj = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'iphone5',
+    }
+  },
+}
+
+export const Tablet: StoryObj = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'ipad',
+    },
+  },
+}
+
+export const Laptop: StoryObj = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'laptop',
+    },
+  },
+}
+
+export const LaptopLarge: StoryObj = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'laptopLarge',
+    },
+  },
+}
