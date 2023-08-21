0fd077bc54526e5696e76dc4ae4c6bcc4ec6c4b2
AugustoCVS <guty.vertolis@gmail.com>
2023-08-21 18:59:32 -0300

    feat: create validation to show the right messages

--- src/app/carteira/components/UpdateWallet/update-wallet.hooks.ts
+++ src/app/carteira/components/UpdateWallet/update-wallet.hooks.ts
@@ -3,12 +3,15 @@
 import { toast } from "react-toastify";
 
 import { updateWallet } from "@/app/carteira/action";
+import { ERROR_UPDATE_MESSAGE, SUCESS_UPDATE_MESSAGE } from "@/utils/constants";
 
 export const useUpdateWallet = () => {
+
   const handleUpdateWallet = async (values: FormData): Promise<void> => {
     try {
       const response = await updateWallet(values);
-      toast(response)
+      if (response === SUCESS_UPDATE_MESSAGE) toast.success(response)
+      if (response === ERROR_UPDATE_MESSAGE) toast.error(response)
     } catch (error) {
       toast.error(`${error}`);
     }
