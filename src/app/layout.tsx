5f03ea77151993bc1cac3e18128897c8d5c7cad8
charlenefialho <fialhocharlene@gmail.com>
2023-08-21 19:40:02 -0300

    feat: added toastContainer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import { Sidebar } from "@/components";
 import "@/styles/tailwind.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +25,7 @@ export default function RootLayout({
       <UserProvider>
         <body className="bg-gray-800">
           <Sidebar />
+          <ToastContainer/>
           {children}
         </body>
       </UserProvider>
