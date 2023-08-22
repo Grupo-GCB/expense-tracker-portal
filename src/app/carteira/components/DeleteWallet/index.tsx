608958f917f6229ac9d8fa4265b3e9e150bc24d9
AugustoCVS <guty.vertolis@gmail.com>
2023-08-22 11:59:13 -0300

    feat: change buttons hover color

--- src/app/carteira/components/DeleteWallet/index.tsx
+++ src/app/carteira/components/DeleteWallet/index.tsx
@@ -15,7 +15,7 @@ export function DeleteWallet({ setOpen }: IUseWallet) {
       <div className="flex flex-col md:flex-row gap-2 md:gap-4 md:w-full md:justify-center">
         <Button
           type="submit"
-          className="py-2 px-4 md:py-4 md:px-6 bg-red-300 w-full md:w-full rounded-md"
+          className="py-2 px-4 md:py-4 md:px-6 bg-red-300 w-full md:w-full rounded-md hover:bg-red-400"
           onClick={actions.handleSaveForm}
           disabled={states.isSavingDataForms}
           canceled={false}
@@ -31,7 +31,7 @@ export function DeleteWallet({ setOpen }: IUseWallet) {
         </Button>
         <Dialog.Close asChild>
           <Button
-            className="py-2 px-4 md:py-4 md:px-6 w-full md:w-full bg-gray-200 rounded-md"
+            className="py-2 px-4 md:py-4 md:px-6 w-full md:w-full bg-gray-200 rounded-md  hover:bg-gray-300"
             canceled
           >
             Cancelar
