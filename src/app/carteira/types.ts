5952115814b08ed7640975811510de4b6ddb1244
AugustoCVS <guty.vertolis@gmail.com>
2023-08-21 21:40:31 -0300

    feat: create wallet schema for validation with zod

--- src/app/carteira/types.ts
+++ src/app/carteira/types.ts
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
+import { z } from "zod";
 
 export interface IUseWallet {
   setOpen: Dispatch<SetStateAction<boolean>>;
@@ -19,3 +20,35 @@ export interface IContentCard{
   titleContent:string
   textContent:string
 }
+
+export const WalletSchema = z.object({
+  account_type: z.string().nonempty({
+    message: "Tipo da conta é obrigatório",
+  }),
+  bank_id: z.string().nonempty({
+    message: "Banco é obrigatório",
+  }),
+  description: z.string().nonempty({
+    message: "Descrição é obrigatório",
+  }),
+});
+
+export type TWallet = z.infer<typeof WalletSchema>;
+
+export interface FieldErrorMapping {
+  [fieldName: string]: {
+    errorMessage: string;
+  };
+}
+
+export const fieldErrorMappings: FieldErrorMapping = {
+  account_type: {
+    errorMessage: "Campo Tipo da conta é obrigatório",
+  },
+  bank_id: {
+    errorMessage: "Campo Banco é obrigatório",
+  },
+  description: {
+    errorMessage: "Campo Descrição é obrigatório",
+  },
+};
