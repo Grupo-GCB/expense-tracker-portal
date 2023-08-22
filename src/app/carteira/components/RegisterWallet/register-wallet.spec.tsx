f8acfabcb243e6abb0234ff6582e79da7a41ca32
AugustoCVS <guty.vertolis@gmail.com>
2023-08-21 22:38:22 -0300

    refactor: fix import

--- src/app/carteira/components/RegisterWallet/register-wallet.spec.tsx
+++ src/app/carteira/components/RegisterWallet/register-wallet.spec.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 
-import  RegisterWallet  from ".";
+import { RegisterWallet }  from "./index";
 
 describe("RegisterWallet", () => {
   it("should be able to render the component correctly", () => {
