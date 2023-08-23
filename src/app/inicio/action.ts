c38464297956ba25256e3a98608fd15b9e85ae72
AugustoCVS <guty.vertolis@gmail.com>
2023-08-23 15:02:25 -0300

    feat: create a registerTransaction feat

---
+++ src/app/inicio/action.ts
@@ -0,0 +1,26 @@
+import { IRegisterTransaction, } from "@/interfaces/Transaction";
+import api from "@/services/api";
+import { AXIOS_ERROR, UNKNOWN_ERROR } from "@/utils/constants";
+import axios from "axios";
+
+export async function registerTransaction(formData: FormData): Promise<string> {
+  try {
+    const {data} = await api.post<IRegisterTransaction>("/transaction", {
+        description: formData.get("description") as string,
+        price: formData.get("price"),
+        bank_id: formData.get("bank_id") as string,
+        date: formData.get("date") as string,
+        wallets: formData.get("wallets") as string,
+        category: formData.get("category") as string,        
+    });
+
+    console.log(data)
+    return "Deu bom";
+  } catch (error) {
+
+    if (axios.isAxiosError(error)) console.error(AXIOS_ERROR, error.message);
+    else console.error(UNKNOWN_ERROR, error);
+
+    return "Deu ruim";
+  }
+}
