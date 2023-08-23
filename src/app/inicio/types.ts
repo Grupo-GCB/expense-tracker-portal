80fcf764b0b2f19a0e1725a33f5b478a5517cf2e
AugustoCVS <guty.vertolis@gmail.com>
2023-08-23 16:01:05 -0300

    feat: create zod schema and error messages

--- src/app/inicio/types.ts
+++ src/app/inicio/types.ts
@@ -1,6 +1,44 @@
 import { Dispatch, SetStateAction } from "react";
+import { z } from "zod";
 
 export interface IUseTransaction {
   setOpen: Dispatch<SetStateAction<boolean>>;
 }
 
+export const newTransactionSchema = z.object({
+  description: z.string().nonempty(),
+  price: z.string().nonempty(),
+  date: z.string().nonempty(),
+  wallets: z.string().nonempty(),
+  category: z.string().nonempty(),
+  type: z.enum(['income', 'outcome'])
+})
+
+export type TTransaction = z.infer<typeof newTransactionSchema>;
+
+export interface FieldErrorMapping {
+  [fieldName: string]: {
+    errorMessage: string;
+  };
+}
+
+export const fieldErrorMappings: FieldErrorMapping = {
+  description: {
+    errorMessage: "Descrição é obirgatório"
+  },
+  price: {
+    errorMessage: "O valor é obirgatório"
+  },
+  date: {
+   errorMessage: "A data é obirgatória"
+  },
+  wallets: {
+    errorMessage: "Uma carteira é obrigatória"
+  },
+  category: {
+    errorMessage: "A categoria é obrigatória"
+  },
+  type: {
+    errorMessage: "Selecionar um tipo é obrigatório"
+  }
+};
