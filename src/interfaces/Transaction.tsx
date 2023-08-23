53901720f16873273faab1ca4dc9efa330c58f53
AugustoCVS <guty.vertolis@gmail.com>
2023-08-23 16:50:55 -0300

    chore: delete useless prop

--- src/interfaces/Transaction.tsx
+++ src/interfaces/Transaction.tsx
@@ -3,7 +3,6 @@ export interface IRegisterTransaction {
     price: number;
     date: string; 
     wallets: string;
-    walletId: string;
     category: string;
     type: 'income' | 'outcome';
   }
