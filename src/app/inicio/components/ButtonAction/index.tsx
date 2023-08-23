4b78f6b4e658ca4b76fdac0e6d1badcb6a35fa1f
charlenefialho <fialhocharlene@gmail.com>
2023-08-23 16:09:36 -0300

    refactor: export interface IButtonAction

--- src/app/inicio/components/ButtonAction/index.tsx
+++ src/app/inicio/components/ButtonAction/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-interface ButtonAction {
+export interface IButtonAction {
   children: React.ReactNode;
 }
 
-export function ButtonAction({ children }: ButtonAction) {
+export function ButtonAction({ children }: IButtonAction) {
   return (
     <button className="focus:outline-green-300 focus:outline-2 ">
       {children}
