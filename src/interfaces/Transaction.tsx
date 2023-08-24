595e5f458b5147efb0a782ecd07fe0be273b72a1
charlenefialho <fialhocharlene@gmail.com>
2023-08-24 16:03:05 -0300

    refactor: rename atrributes ITransaction

--- src/interfaces/Transaction.tsx
+++ src/interfaces/Transaction.tsx
@@ -1,13 +1,10 @@
 export interface ITransaction {
-  categories: string;
-  description: string;
-  value: string;
-  type: "Despesa" | "Receita";
-  date: string;
+  transaction_categories: string;
+  transaction_description: string;
+  transaction_value: string;
+  transaction_type: "Despesa" | "Receita";
+  transaction_date: string;
   wallet_id: string;
-  name_bank: string;
-  updated_at: string;
-  deleted_at: boolean | null;
-  id: string;
-  created_at: string;
+  bank_name: string;
+  transaction_id: string;
 }
