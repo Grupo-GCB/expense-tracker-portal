b669b884f417da5887a99d70e7891dc7249138b4
AugustoCVS <guty.vertolis@gmail.com>
2023-08-24 11:54:19 -0300

    feat: create transactionTypes component

---
+++ src/app/inicio/components/RegisterTransaction/components/TransactionTypes/index.tsx
@@ -0,0 +1,37 @@
+import * as RadioGroup from "@radix-ui/react-radio-group";
+import { ArrowCircleUp, ArrowCircleDown } from "phosphor-react";
+
+import { ITransactionType } from "@/interfaces";
+
+export default function TransactionTypes({
+  value,
+  selectedOption,
+  handleOptionSelect,
+}: ITransactionType) {
+  return (
+    <RadioGroup.Item
+      className={`p-4 flex items-center justify-center gap-2 rounded cursor-pointer decoration-gray-300 font-normal ${
+        selectedOption === value
+          ? value === "Receita"
+            ? "bg-green-500"
+            : "bg-red-300"
+          : "bg-gray-700 hover:bg-gray-300"
+      }`}
+      value={value}
+      onClick={() => handleOptionSelect(value)}
+    >
+      {value === "Receita" ? (
+        <ArrowCircleUp
+          size={20}
+          color={`${selectedOption === value ? "#FFFFFF" : "#00B37E"}`}
+        />
+      ) : (
+        <ArrowCircleDown
+          size={20}
+          color={`${selectedOption === value ? "#FFFFFF" : "#F75A68"}`}
+        />
+      )}
+      {value === "Receita" ? "Entrada" : "Saída"}
+    </RadioGroup.Item>
+  );
+}
