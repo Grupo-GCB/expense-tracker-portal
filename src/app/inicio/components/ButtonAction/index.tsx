e84941ab1638b1fe8a1c05c2d59eb7329f50c153
charlenefialho <fialhocharlene@gmail.com>
2023-08-24 14:36:00 -0300

    refactor: extends button attributes

--- src/app/inicio/components/ButtonAction/index.tsx
+++ src/app/inicio/components/ButtonAction/index.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { HTMLAttributes } from "react";
 
-export interface IButtonAction {
+export interface IButtonAction extends HTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  onClick: () => void;
 }
 
-export function ButtonAction({ children }: IButtonAction) {
+export function ButtonAction({ children, onClick }: IButtonAction) {
   return (
-    <button className="focus:outline-green-300 focus:outline-2 ">
+    <button
+      className="focus:outline-green-300 focus:outline-2 "
+      onClick={onClick}
+    >
       {children}
     </button>
   );
