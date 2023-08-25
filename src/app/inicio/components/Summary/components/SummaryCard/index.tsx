743dd96b7ddffc6df37533e70747b555068c2b07
AugustoCVS <guty.vertolis@gmail.com>
2023-08-25 17:26:41 -0300

    feat: import useSummary hook

--- src/app/inicio/components/Summary/components/SummaryCard/index.tsx
+++ src/app/inicio/components/Summary/components/SummaryCard/index.tsx
@@ -1,25 +1,33 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { useSummary } from "../../hook";
+import { priceFormatter } from "@/utils";
 
 export default function SummaryCard() {
+  const summary = useSummary();
+
+  function ChangeTotalBackground() {
+    if (summary.total >= 0) return "bg-green-700";
+    else return "bg-red-700";
+  }
 
   const summaryOptions = [
     {
       id: 1,
       title: "Entradas",
       icon: <ArrowCircleUp size={32} color="#00b37e" />,
-      value: 1000
+      value: priceFormatter.format(summary.income),
     },
     {
       id: 2,
       title: "Saídas",
       icon: <ArrowCircleDown size={32} color="#f75a68" />,
-      value: 1000
+      value: priceFormatter.format(summary.outcome),
     },
     {
       id: 3,
       title: "Total",
       icon: <CurrencyDollar size={32} color="#fff" />,
-      value: 1000
+      value: priceFormatter.format(summary.total),
     },
   ];
 
@@ -27,7 +35,7 @@ export default function SummaryCard() {
     <div className="w-full max-w-screen-xl mx-auto px-6 grid grid-cols-3 gap-8 mt-[-5rem]">
       {summaryOptions.map((options) => {
         const isTotal = options.title === "Total";
-        const bgClass = isTotal ? "bg-green-700" : "bg-gray-600";
+        const bgClass = isTotal ? `${ChangeTotalBackground()}` : "bg-gray-600";
 
         return (
           <div key={options.id} className={`${bgClass} rounded-sm p-8`}>
