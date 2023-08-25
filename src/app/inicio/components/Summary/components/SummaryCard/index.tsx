a8cd6313352fae18341a2a875bb01d19082fd1be
AugustoCVS <guty.vertolis@gmail.com>
2023-08-25 15:10:38 -0300

    feat: create summary component

---
+++ src/app/inicio/components/Summary/components/SummaryCard/index.tsx
@@ -0,0 +1,46 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+
+export default function SummaryCard() {
+
+  const summaryOptions = [
+    {
+      id: 1,
+      title: "Entradas",
+      icon: <ArrowCircleUp size={32} color="#00b37e" />,
+      value: 1000
+    },
+    {
+      id: 2,
+      title: "Saídas",
+      icon: <ArrowCircleDown size={32} color="#f75a68" />,
+      value: 1000
+    },
+    {
+      id: 3,
+      title: "Total",
+      icon: <CurrencyDollar size={32} color="#fff" />,
+      value: 1000
+    },
+  ];
+
+  return (
+    <div className="w-full max-w-screen-xl mx-auto px-6 grid grid-cols-3 gap-8 mt-[-5rem]">
+      {summaryOptions.map((options) => {
+        const isTotal = options.title === "Total";
+        const bgClass = isTotal ? "bg-green-700" : "bg-gray-600";
+
+        return (
+          <div key={options.id} className={`${bgClass} rounded-sm p-8`}>
+            <header className="flex items-center justify-between text-gray-100">
+              <span className="text-2xl">{options.title}</span>
+              {options.icon}
+            </header>
+            <strong className="block mt-4 text-4xl text-gray-100">
+              {options.value}
+            </strong>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
