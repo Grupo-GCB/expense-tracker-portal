88aca18ff819d6f1ca1b6ea832eda87d378700a4
charlenefialho <fialhocharlene@gmail.com>
2023-08-28 10:36:07 -0300

    refactor: change padding of TableTransactionColumn

--- src/app/inicio/components/TableTransaction/TableTrasactionColumn.tsx
+++ src/app/inicio/components/TableTransaction/TableTrasactionColumn.tsx
@@ -1,9 +1,7 @@
-interface ITableTransactionContent{
-  children: React.ReactNode
+interface ITableTransactionContent {
+  children: React.ReactNode;
 }
 
-export function TableTransactionColumn({children}: ITableTransactionContent){
-  return(
-    <td className=" px-5 py-8 bg-gray-700">{children}</td>
-  )
+export function TableTransactionColumn({ children }: ITableTransactionContent) {
+  return <td className="py-5 px-8  bg-gray-700">{children}</td>;
 }
