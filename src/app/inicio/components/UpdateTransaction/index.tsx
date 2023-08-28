8e38e26f5c7c59bb80dfddd3882fff46f1f5cfcb
charlenefialho <fialhocharlene@gmail.com>
2023-08-28 15:15:46 -0300

    feat: added form update transaction

--- src/app/inicio/components/UpdateTransaction/index.tsx
+++ src/app/inicio/components/UpdateTransaction/index.tsx
@@ -1,56 +1,95 @@
-import * as Dialog from "@radix-ui/react-dialog";
 import { CircleNotch } from "phosphor-react";
+import { Dispatch, SetStateAction, useState } from "react";
 
-import { Button, CustomSelect, FormModal } from "@/components";
-import { accountTypes } from "@/utils/constants";
+import { Button, CustomSelect, FormModal, Input } from "@/components";
+import { getCurrentDate } from "@/utils";
+import { categoryTypes } from "@/utils/constants";
+import TransactionTypes from "../RegisterTransaction/components/TransactionTypes";
+import { useUpdateTransaction } from "./hook";
 
-export function UpdateTransaction() {
-  const {updateActions, states} = useUpdateTransaction()
+interface IUseTransaction {
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  idTransaction: string;
+}
+
+export function UpdateTransaction({ setOpen, idTransaction }: IUseTransaction) {
+  const { updateActions, states } = useUpdateTransaction({ setOpen });
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+  const handleOptionSelect = (value: string): void => {
+    if (selectedOption === value) setSelectedOption(null);
+    else setSelectedOption(value);
+  };
   return (
-    <FormModal action={() => {}}>
-      <CustomSelect
-        options={accountTypes}
-        placeholder="Tipo da conta"
-        name="account_type"
-      />
-      <CustomSelect
-        options={[]}
-        placeholder="Banco"
-        name="bank_id"
-      />
-      <textarea
-        name="description"
+    <FormModal action={updateActions.handleUpdateTransaction}>
+      <Input.Root
         id="description"
-        cols={30}
-        rows={3}
-        maxLength={32}
+        name="description"
         placeholder="Descrição"
-        className="mb-3 md:mb-10 bg-gray-900 pl-4 pt-4 rounded ou focus:outline-none focus:ring focus:ring-green-300 resize-none"
-      ></textarea>
+        maxLength={32}
+        className="flex items-center px-4 bg-gray-900 h-12 rounded focus:outline-none focus:ring focus:ring-green-300 resize-none"
+      />
+      <Input.Root
+        id="value"
+        name="value"
+        type="number"
+        min={0}
+        placeholder="Preço"
+        maxLength={32}
+        className="flex items-center px-4 bg-gray-900 h-12 rounded focus:outline-none focus:ring focus:ring-green-300 resize-none"
+      />
+      <Input.Root
+        id="date"
+        name="date"
+        type="date"
+        maxLength={32}
+        placeholder="Data"
+        max={getCurrentDate()}
+        className="flex items-center px-4 bg-gray-900 h-12 rounded focus:outline-none focus:ring focus:ring-green-300 resize-none  decoration-gray-300"
+      />
+      <CustomSelect
+        id="wallets"
+        name="wallets"
+        options={states.walletsNames}
+        placeholder="Carteiras"
+      />
+
+      <CustomSelect
+        id="categories"
+        name="categories"
+        options={categoryTypes}
+        placeholder="Categoria"
+      />
+
+      <div className="sm:grid sm:grid-rows-1 sm:w-full md:grid md:grid-cols-2 gap-4 mt-2">
+        <TransactionTypes
+          value="Receita"
+          selectedOption={selectedOption}
+          handleOptionSelect={handleOptionSelect}
+        />
+        <TransactionTypes
+          value="Despesa"
+          selectedOption={selectedOption}
+          handleOptionSelect={handleOptionSelect}
+        />
+      </div>
+
       <div className="flex flex-col md:flex-row gap-2 md:gap-4 md:w-full md:justify-center">
         <Button
           type="submit"
           className="py-2 px-4 md:py-4 md:px-6 bg-green-500 w-full md:w-full rounded-[6px]"
-          disabled={false}
+          disabled={states.isSavingDataForms}
           canceled={false}
         >
-          {states ? (
+          {states.isSavingDataForms ? (
             <CircleNotch
               className="animate-spin w-full justify-center"
               data-testid="loading-icon"
             />
           ) : (
-            <span>Confirmar!</span>
+            <span>Confirmar</span>
           )}
         </Button>
-        <Dialog.Close asChild>
-          <Button
-            className="py-2 px-4 md:py-4 md:px-6 w-full md:w-full rounded-[6px]"
-            canceled
-          >
-            Cancelar
-          </Button>
-        </Dialog.Close>
       </div>
     </FormModal>
   );
