a5e626598bdb2ad0a2fde863e840e640ba31e6f1
AugustoCVS <guty.vertolis@gmail.com>
2023-08-29 14:33:43 -0300

    feat: feat to verify if the transaction is income

--- src/app/inicio/components/Summary/hook.ts
+++ src/app/inicio/components/Summary/hook.ts
@@ -3,23 +3,23 @@
 import { useMemo } from "react";
 
 import { useHome } from "@/app/inicio/hook";
+import { ITransaction } from "@/interfaces";
 
 export const useSummary = () => {
   const { states } = useHome();
+
+  const isIncomeTransaction = (transaction: ITransaction): boolean => {
+    return transaction.type === "Receita";
+  };
 
   const summary = useMemo(() => {
     return states.transactions.reduce(
       (acc, transaction) => {
         const transactionPrice = parseFloat(transaction.value);
 
-        if (transaction.type === "Receita") {
-          acc.income += transactionPrice;
-          acc.total += transactionPrice;
-        } else {
-          acc.outcome += transactionPrice;
-          acc.total += transactionPrice;
-        }
-
+        acc.income += isIncomeTransaction(transaction) ? transactionPrice : 0;
+        acc.outcome += isIncomeTransaction(transaction) ? 0 : transactionPrice;
+        acc.total += transactionPrice;
         return acc;
       },
       {
