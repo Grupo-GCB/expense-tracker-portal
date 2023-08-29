b1ca0560974012d2903d50ba067f207fbe2a7599
AugustoCVS <guty.vertolis@gmail.com>
2023-08-29 16:52:58 -0300

    feat: create feat to call the withoutWallet modal

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -3,10 +3,10 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 import axios from "axios";
 import { parseCookies, setCookie } from "nookies";
-import { CircleNotch } from "phosphor-react";
-import { useCallback, useEffect } from "react";
+import { CircleNotch, Warning } from "phosphor-react";
+import { useCallback, useEffect, useState } from "react";
 
-import { ErrorPage } from "@/components";
+import { ErrorPage, Modal } from "@/components";
 import { ISignInResponse, IToken, ITransactionList } from "@/interfaces";
 import api from "@/services/api";
 import getUserSession from "@/services/userSession";
@@ -21,11 +21,18 @@ import { TableTransaction } from "./components/TableTransaction";
 import { TableTransactionContent } from "./components/TableTransaction/TableTransactionContent";
 import { useHome } from "./hook";
 import Summary from "./components/Summary";
+import WithoutWallet from "./components/WithoutWalltes";
+import { useShowAllWallet } from "@/app/carteira/components/CardWallet/hook";
 
 export default function Home() {
+  const { walletStates } = useShowAllWallet()
   const { states } = useHome();
-
   const { user } = useUser();
+  const [open, setOpen] = useState<boolean>(verifyIfWalletsExist())
+
+  function verifyIfWalletsExist(): boolean {
+    return walletStates.walletList.length === 0;
+  }
 
   async function sendToken({ token }: IToken): Promise<void> {
     try {
@@ -85,6 +92,22 @@ export default function Home() {
       <div className="lg:ml-28">
         <Header />
         <Summary />
+      </div>
+      <div>
+        <Modal open={open} onOpenChange={setOpen}>
+          <Modal.Content>
+            <div className="flex items-center justify-center flex-col gap-4">
+              <div className="flex items-center justify-center bg-red-300 w-16 h-16 rounded-full">
+                <Warning size={40} />
+              </div>
+              <div className="flex items-center justify-center flex-col">
+                <h2>Você ainda não possui carteiras</h2>
+                <p>Gostaria de ir para a página de carteiras ?</p>
+              </div>
+            </div>
+            <WithoutWallet setOpen={setOpen}/>
+          </Modal.Content>
+        </Modal>
       </div>
       <main className="w-full max-w-screen-xl mx-auto sm:justify-center sm:px-2  sm:mt-8 text-white">
         {states.transactions.length === 0 && (
