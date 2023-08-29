ded03ea237ae0cc7cb537eabb8ff502225dcc6bb
AugustoCVS <guty.vertolis@gmail.com>
2023-08-29 17:46:59 -0300

    feat: change to useRouter

--- src/app/inicio/components/WithoutWalltes/index.tsx
+++ src/app/inicio/components/WithoutWalltes/index.tsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 
 import * as Dialog from "@radix-ui/react-dialog";
-import router from "next/router";
+import { useRouter } from "next/navigation";
 
 import { Button, FormModal } from "@/components";
 import { IUseTransaction } from "@/app/inicio/types";
 
-export default function WithoutWallet({setOpen}: IUseTransaction) {
-
+export default function WithoutWallet({ setOpen }: IUseTransaction) {
+  const router = useRouter();
   return (
     <FormModal>
       <div className="flex flex-col md:flex-row gap-2 md:gap-4 md:w-full md:justify-center">
@@ -15,7 +15,7 @@ export default function WithoutWallet({setOpen}: IUseTransaction) {
           type="submit"
           className="py-2 px-4 md:py-4 md:px-6 bg-red-300 w-full md:w-full rounded-md hover:bg-red-400"
           canceled={false}
-          onClick={() => {router.push("carteira")}}
+          onClick={() => router.push("carteira")}
         >
           Confirmar
         </Button>
@@ -23,7 +23,9 @@ export default function WithoutWallet({setOpen}: IUseTransaction) {
           <Button
             className="py-2 px-4 md:py-4 md:px-6 w-full md:w-full bg-gray-200 rounded-md  hover:bg-gray-300"
             canceled
-            onClick={() => {setOpen(false)}}
+            onClick={() => {
+              setOpen(false);
+            }}
           >
             Cancelar
           </Button>
