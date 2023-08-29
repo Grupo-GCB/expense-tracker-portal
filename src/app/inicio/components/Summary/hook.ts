7299b592b3c53ba1fb566bb62f43b0085540c8d5
AugustoCVS <guty.vertolis@gmail.com>
2023-08-29 14:42:58 -0300

    feat: create a feat to calculate the summary

--- src/app/inicio/components/Summary/hook.ts
+++ src/app/inicio/components/Summary/hook.ts
@@ -3,7 +3,7 @@
 import { useMemo } from "react";
 
 import { useHome } from "@/app/inicio/hook";
-import { ITransaction } from "@/interfaces";
+import { ISummary, ITransaction } from "@/interfaces";
 
 export const useSummary = () => {
   const { states } = useHome();
@@ -12,14 +12,15 @@ export const useSummary = () => {
     return transaction.type === "Receita";
   };
 
-  const summary = useMemo(() => {
-    return states.transactions.reduce(
+  const calculateSummary = (transaction: ITransaction[]): ISummary => {
+    return transaction.reduce(
       (acc, transaction) => {
         const transactionPrice = parseFloat(transaction.value);
 
         acc.income += isIncomeTransaction(transaction) ? transactionPrice : 0;
         acc.outcome += isIncomeTransaction(transaction) ? 0 : transactionPrice;
         acc.total += transactionPrice;
+
         return acc;
       },
       {
@@ -28,6 +29,10 @@ export const useSummary = () => {
         total: 0,
       }
     );
+  };
+
+  const summary = useMemo(() => {
+    return calculateSummary(states.transactions);
   }, [states.transactions]);
 
   return summary;
