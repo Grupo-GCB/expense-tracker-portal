80c16e7bec31c1552dcf8f8bfd483a6df74ded99
AugustoCVS <guty.vertolis@gmail.com>
2023-08-30 13:53:19 -0300

    feat: change onSubmit logic

--- src/components/Modal/FormModal.tsx
+++ src/components/Modal/FormModal.tsx
@@ -1,15 +1,22 @@
-import { HTMLAttributes } from "react";
+import { FormEvent, HTMLAttributes } from "react";
 
 interface IFormModal extends HTMLAttributes<HTMLFormElement> {
   action?: (formData: FormData) => void;
 }
 
-export function FormModal({ children, action, onSubmit }: IFormModal) {
+export function FormModal({ children, action }: IFormModal) {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (action) {
+      const formData = new FormData(e.target as HTMLFormElement);
+      action(formData);
+    }
+  };
+
   return (
     <form
-      action={action}
+      onSubmit={handleSubmit}
       className="flex flex-col gap-4 mt-5 md:mt-10 w-full"
-      onSubmit={onSubmit}
     >
       {children}
     </form>
