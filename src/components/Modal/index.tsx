2e95049941fd7db3f113ba841cd13a4e7d230356
charlenefialho <fialhocharlene@gmail.com>
2023-08-30 16:43:54 -0300

    feat: added style on scrollbar

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -23,7 +23,7 @@ function ModalContent({ children }: { children: ReactNode }) {
     <Dialog.Portal>
       <Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
       <Dialog.Content>
-        <div className="w-10/12 md:w-1/2 bg-gray-800 text-white fixed left-1/2 top-1/2  max-w-md -translate-x-1/2 -translate-y-1/2 rounded-[6px] p-8 shadow data-[state=closed]:animate-[dialog-content-hide_200ms] data-[state=open]:animate-[dialog-content-show_200ms]">
+        <div className="w-10/12 md:w-1/2 bg-gray-800 text-white fixed left-1/2 top-1/2  max-h-[90%] overflow-y-scroll max-w-md -translate-x-1/2 -translate-y-1/2 rounded-[6px] p-8 shadow data-[state=closed]:animate-[dialog-content-hide_200ms] data-[state=open]:animate-[dialog-content-show_200ms] scrollbar-thin scrollbar-zinc-950 scrollbar-thumb-800">
           <div className="flex justify-end">
             <Dialog.Close asChild>
               <button className="focus:outline-none focus:ring focus:ring-green-300 ">
